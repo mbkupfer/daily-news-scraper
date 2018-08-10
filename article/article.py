f714e13858844c48728d92e3ebb8cc9a4f3892dd
Maxim Kupfer <mbkupfer@gmail.com>
2018-08-10 15:02:51 +0000

    Added focus text to introduce text processing

--- article/article.py
+++ article/article.py
@@ -10,6 +10,24 @@ mapping = {
     'eng':  'EMEA',
     'row':  'APAC'
 }
+
+def focus_text(sentence, focus_words):
+    """
+    Returns a string with focus words capitalized
+
+    :sentence(str): we will scan this and modify if words match
+    :focus_words: an array of words to capitalize
+    
+    """
+    sentence = sentence.lower()
+    for w in focus_words:
+        sentence = sentence.replace(w, w.upper())
+    return sentence
+
+    
+focus_words = [
+    'merger', 'acquisition', 'acquires', 'buys', 'purchase'
+    ]
 with open('articles.txt', 'wt+') as file:
     for region in regions:
         r = requests.get('https://www2.staffingindustry.com/{}/Editorial/Daily-News'.format(region))
@@ -21,13 +39,14 @@ with open('articles.txt', 'wt+') as file:
             article_r = requests.get(BASE_URL + link)
             article_dom = BeautifulSoup(article_r.text, 'html.parser')
             article_title = article_dom.find(id='site-main-content').header.h1.string
-            print('Caputring data for article: {}'.format(article_title))
+            print('Capturing: {}'.format(article_title))
             article_date = article_dom.find(id='site-main-content').find_all('div')[2].string
             article_text = article_dom.find(id='site-main-content').find_all('p')
             file.write(article_title + '\n\n')
             file.write(article_date + '\n\n')
             for p in article_text:
-                file.write(p.text + '\n\n')
+                s = focus_text(p.text, focus_words)
+                file.write(s + '\n\n')
             file.write('\n\n [END OF ARTICLE] \n\n')
 
 # article: <div class="content-view-line class-news-item">
